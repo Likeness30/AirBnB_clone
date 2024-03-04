8022227d5ee632df4ba17a2e9f44d6e1ddb1404e
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-04 11:20:17 +0100

    console.py

--- console.py
+++ console.py
@@ -1,58 +1,10 @@
-# AirBnB Clone Command Interpreter
+#!/usr/bin/python3
 
-Welcome to the AirBnB clone project! This project involves building a command-line interface (CLI) to manage AirBnB objects. The command interpreter will allow users to perform various operations such as creating, retrieving, updating, and deleting objects like users, states, cities, and places within the AirBnB system.
-
-![AirBnB Image](assets/airbnb.png)
-
-## Introduction
-
-The primary goal of this project is to create a command-line interface that interacts with objects in an AirBnB-like application. The command interpreter will provide a simple yet powerful way to manage these objects, allowing users to perform CRUD (Create, Read, Update, Delete) operations.
-
-## Description
-
-This project is an implementation of a command-line interface (CLI) for managing AirBnB-like objects. The command interpreter allows users to perform various operations such as creating, retrieving, updating, and deleting objects within the AirBnB system.
-
-## Command Interpreter
-
-### Description
-
-The command interpreter provides a shell-like interface for interacting with AirBnB objects. It allows users to execute commands to perform operations on objects such as users, states, cities, and places.
-
-### How to Start
-
-To start the command interpreter, follow these steps:
-
-1. Clone the project repository from GitHub.
-2. Navigate to the project directory in your terminal.
-3. Run the command `./console.py` to start the command interpreter.
-
-### How to Use
-
-Once the command interpreter is running, you can use it to perform various operations on AirBnB objects. Here are the available commands:
-
-- **create**: Create a new instance of an object.
-- **show**: Show details of a specific object.
-- **all**: List all instances of a specific class or all classes.
-- **update**: Update attributes of an object.
-- **destroy**: Delete an object.
-- **quit** or **exit**: Exit the command interpreter.
-
-### Examples
-
-Here are some examples of how to use the command interpreter:
-
-1. **Create a new user**:
-
-2. **Show details of a specific place**:
-
-3. **List all users**:
-
-4. **Update the name of a city**:
-
-5. **Delete a state**:
-
-6. **Exit the command interpreter**:
-
-## Conclusion
-
-The AirBnB command interpreter provides a convenient and efficient way to manage objects within the AirBnB system. With its intuitive interface and versatile functionality, users can easily perform various operations on AirBnB objects, making it an essential tool for developers working on AirBnB-like applications.
+if __name__ == "__main__":
+    while (True):
+        command = input("(hbnb)")
+        if command == "exit":
+            break
+        elif command == "help":
+            print("help")
+    

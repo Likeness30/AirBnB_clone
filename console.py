122b72218a1c6f6d4211e0f2f25b9ea2a6e9d9fe
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-05 08:57:53 +0100

    console.py

--- console.py
+++ console.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+import cmd
+
+
+class CommandLine(cmd.Cmd):
+    """A simple Command interpreter"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Exits the cmd"""
+        return (True)
+
+    def do_EOF(self, args):
+        """EOF"""
+        return (True)
+
+    def emptyline(self):
+        pass
+
 
 if __name__ == "__main__":
-    while (True):
-        command = input("(hbnb)")
-        if command == "exit":
-            break
-        elif command == "help":
-            print("help")
-    
+    print(dir(cmd.Cmd))
+    CommandLine().cmdloop()

91960dcd4e81062f1a432dc4e42ef3325dc8ace0
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-10 17:00:55 +0100

    console.py

--- console.py
+++ console.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
+"""this is the default console module"""
 import cmd
 import sys
 
 
 class HBNBCommand(cmd.Cmd):
     """A simple Command interpreter"""
-
-    def __init__(self, interactive=True):
-        super().__init__()
-        self.is_interactive = interactive
-        self.prompt = "(hbnb) " if interactive else "(hbnb)\n"
 
     def do_quit(self, args):
         """Exits the cmd"""
@@ -21,15 +17,6 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
-
-    def precmd(self, line):
-        return line.strip()
-
-    def postcmd(self, stop, line):
-        if not self.is_interactive:
-            print("")
-            print("(hbnb)")
-        return stop
 
 
 if __name__ == "__main__":

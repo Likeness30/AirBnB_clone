897141ff6841c790356ae5d4aa49f255a97437e2
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-11 01:51:36 +0100

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines the BaseModel class"""
 from uuid import uuid4
 import datetime
-from models.__init__ import storage
+import models
 class BaseModel:
     """BaseModel"""
 
@@ -11,7 +11,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
+                    if key in ['created_at', 'updated_at']:
                         setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                     else:
                         setattr(self, key, value)
@@ -19,9 +19,8 @@ class BaseModel:
             self.name = kwargs.get('name', None)
             self.my_number = kwargs.get('my_number', None)
             self.id = str(uuid4())
-            self.created_at = datetime.datetime.now().isoformat()
+            self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
-
 
     def __str__(self):
         """Returns official string representation"""
@@ -29,8 +28,9 @@ class BaseModel:
 
     def save(self):
         """update the public instace attribute updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now().isoformat()
-        storage.save(self)
+        self.__dict__["updated_at"] = datetime.datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance"""
@@ -39,8 +39,8 @@ class BaseModel:
                 'my_number': self.my_number,
                 'name': self.name,
                 '__class__': self.__class__.__name__,
-                'updated_at': self.updated_at,
+                'updated_at': self.updated_at.isoformat(),
                 'id': self.id,
-                'created_at': self.created_at
+                'created_at': self.created_at.isoformat()
             }
         )
--- models/user.py
+++ models/user.py
@@ -4,9 +4,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing user objects"""
+    """User class"""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """Initialize User attributes"""
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")
